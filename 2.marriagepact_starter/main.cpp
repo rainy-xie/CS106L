4032abd2bc10669244da4113113f650ead88f399
Rainy.Xie <1972575545@qq.com>
2024-03-04 14:06:40 +0800

    assignment 02 done

--- 2.marriagepact_starter/main.cpp
+++ 2.marriagepact_starter/main.cpp
@@ -1,30 +1,82 @@
-/* 
+/*
 Assignment 2: Marriage Pact
 
-This assignment asks you to discover your one true love using containers and pointers.
-There are two parts: implement get_applicants and find_matches.
+This assignment asks you to discover your one true love using containers and
+pointers. There are two parts: implement get_applicants and find_matches.
 
 Submit to Paperless by 11:59pm on 2/1/2024.
 */
 
+#include <fstream>
 #include <iostream>
-#include <string>
+#include <list>
 #include <queue>
 #include <set>
+#include <sstream>
+#include <string>
+
+std::string STUDENT_PATH = "students.txt";
+
+void print_applicants(std::set<std::string> applicants);
 
 std::set<std::string> get_applicants(std::string filename) {
-    // TODO: Implement this function. Feel free to change std::set to std::unordered_set if you wish!
-
+  // TODO: Implement this function. Feel free to change std::set to
+  // std::unordered_set if you wish!
+  std::ifstream ifs(filename);
+  if (!ifs.is_open()) {
+    std::cerr << "Failed to open file: " << filename << std::endl;
+  }
+  std::string name;
+  std::set<std::string> applicants;
+  while (std::getline(ifs, name)) {
+    applicants.insert(name);
+  }
+  return applicants;
 }
 
-std::queue<std::string*> find_matches(std::set<std::string> &students) {
-    // TODO: Implement this function.
+std::queue<const std::string *, std::list<const std::string *>>
+find_matches(std::set<std::string> &students) {
+  // TODO: Implement this function.
+  //   print_applicants(students);
+  // std::queue<const std::string *> matches;
+  std::queue<const std::string *, std::list<const std::string *>> matches;
+  for (auto &student : students) {
+    std::stringstream ss(student);
+    std::string first, last;
+    ss >> first >> last;
+    // std::cout << "first = " << first << ", "
+    //           << "last = " << last << std::endl;
+    // if (first[0] == 'R' && last[0] == 'X') {
+    if (first[0] == 'A' && last[0] == 'X') {
+      // if (last[0] == 'X') {
+      matches.push(&student);
+    }
+  }
+  return matches;
 }
 
+void print_applicants(std::set<std::string> applicants) {
+  for (auto it = applicants.begin(); it != applicants.end(); ++it) {
+    std::cout << *it << std::endl;
+  }
+}
 
-
+void print_matches(std::queue<const std::string *,std::list<const std::string *>> matches) {
+  if (matches.empty()) {
+    std::cout << "NO STUDENT FOUND." << std::endl;
+    return;
+  }
+  while (!matches.empty()) {
+    std::cout << *matches.front() << std::endl;
+    matches.pop();
+  }
+}
 int main() {
-    // Your code goes here. Don't forget to print your true love!
-    
-    return 0;
+  // Your code goes here. Don't forget to print your true love!
+  auto applicants = get_applicants(STUDENT_PATH);
+  auto matches = find_matches(applicants);
+
+  //   print_applicants(applicants);
+  print_matches(matches);
+  return 0;
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # Week Topics
 
-
 1 Admin, Brief Intro to C++ feature
 2 Initialization + References, Streams
 3 Containers, Iterators, Pointers
@@ -8,3 +7,13 @@
 5 Template Functions, Functions, Lambdas
 6 Operators, Special Member Functions
 7 Move Semantics, Type safety
+
+# Assignment
+
+1.simpleenroll_starter
+使用输入输出流处理csv文件
+
+2.marriagepact_starter
+set、queue容器的简单使用
+
+# note
